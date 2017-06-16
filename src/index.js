c2702706f3db6ebafcdd487b11373a94d7882954
Doug Fitzmaurice <doug.fitzmaurice@ents24.com>
2017-06-16 14:02:33 +0100

    More interface

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var m = require("mithril");
 
 var Wiki = {};
 Wiki.Nav = {};
+Wiki.Articles = {};
 Wiki.Nav.vm = {
 	list: [],
 	init: function() {
@@ -16,6 +17,15 @@ Wiki.Nav.vm = {
 			console.log(result);
 		    Wiki.Nav.vm.list = result;
 		});
+	},
+	handleClick: function(e) {
+		e.preventDefault();
+		var targetUrl = e.target.href;
+		var targetHost = e.target.hostname;
+		var parts = e.target.href.split(targetHost);
+		var relativeUrl = parts[1];
+		
+		Wiki.Articles.vm.load(relativeUrl);
 	}
 };
 Wiki.Nav.View = {
@@ -24,9 +34,16 @@ Wiki.Nav.View = {
 	},
 	view: function() {
 		console.log(Wiki.Nav);
-		return m("ul#tree", Wiki.Nav.vm.list.map(function(entry) {
-			return m("li.file", m("a", {href: entry.path}, entry.path));
-		}));
+		return m("div.inner", [
+			m("h2", m("span", "slimWiki")),
+			m("div#tree-filter.input-group", [
+				m("input#tree-filter-query.form-control.input-sm", {placeholder: "Search", type: "text"}),
+				m("a#tree-filter-clear-query.input-group-addon.input-sm", m("i.glyphicon.glyphicon-remove")),
+			]),
+			m("ul#tree", Wiki.Nav.vm.list.map(function(entry) {
+				return m("li.file", m("a", {href: entry.path, onclick: Wiki.Nav.vm.handleClick}, entry.path));
+			}))
+		]);
 	}
 };
 
